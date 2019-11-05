5f54cb3750c4c74307c7d5180a689fd66d6f5e28
Guilherme Cazaroto <gui.cazaroto@gmail.com>
2019-11-05 19:01:34 -0300

    authentication

--- routes/index.php
+++ routes/index.php
@@ -1,8 +1,16 @@
 <?php
 use function src\slimConfiguration;
 use App\Controllers\StoreController;
+use Tuupola\Middleware\HttpBasicAuthentication;
 
 $app = new \Slim\App(slimConfiguration());
+$app->add(new HttpBasicAuthentication([
+  "secure" => false,
+  "users" => [
+      "root" => "t00r",
+      "somebody" => "passw0rd"
+  ]
+]));
 
 $app->get('/store' , StoreController :: class . ':getAllStores');
 $app->post('/store', StoreController :: class . ':createStore');
